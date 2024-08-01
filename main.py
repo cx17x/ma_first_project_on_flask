264ec4777afcea2d01e53bfcf74439d98d700ae4
cx17x <gogadlyavangoga@mail.ru>
2024-08-01 21:02:04 +0300

    Resolved merge conflicts

---
+++ main.py
@@ -0,0 +1,67 @@
+# Короче на фласк/фастапи(в синхронном режиме)
+# Хранить все в обычном списке
+# 4 метода
+# /govno GET
+# Отдает случайный эмодзи какашки строкой. Чтобы было как минимум 3 варианта по умолчанию
+# /govno/<id> GET
+# Из ссылки забрать id, отдать какашку с соотвествующим порядковым номером. Если такого id нет(вышел за пределы) - отдать 404 код
+# /govno/<id> PUT
+# Заменяет эмодзи какашки с переданным порядковым номером на присланное. Если id вышел  за границы - 404. Проверить что заслали именно эмодзи, если нет - 400 ошибка отдается
+# /govno POST
+# Принимает эмодзи и кладет его в список. Если такой уже есть - отдает 409 код ошибки
+
+# 🐯 🐰 🐱 🐲 🐳 🐴 🐵 🐶 🐷
+import random
+from flask import Flask, jsonify, request
+from emoji import is_emoji
+from manager import Manager
+
+app = Flask(__name__)
+
+govno_emojis = ['💩', '🤢', '🤮']
+
+
+@app.route('/govno', methods=['GET'])
+def get_random_govno():
+    return jsonify(random.choice(govno_emojis))
+
+
+@app.route('/govno/<int:id>', methods=['GET'])
+def get_govno_by_id(id):
+    if id < 0 or id >= len(govno_emojis):
+        return jsonify({'error': 'Not found'}, 404)
+    return jsonify(govno_emojis[id])
+
+
+@app.route('/govno/<int:id>', methods=['PUT'])
+def update_govno_by_id(id:int):
+    if id < 0 or id >= len(govno_emojis):
+        return jsonify({'error': 'Not found'}), 404
+    new_emoji = request.json.get('emoji')
+    if not new_emoji or len(new_emoji) != 1:
+        return jsonify({'error': 'Bad request'}), 400
+    govno_emojis[id] = new_emoji
+    return jsonify(govno_emojis[id])
+
+
+@app.route('/govno', methods=['POST'])
+def add_govno():
+    new_emoji = request.json.get('emoji')
+    if not new_emoji or len(new_emoji) != 1:
+        return jsonify({'error': 'Bad request'}), 400
+    if not is_emoji(new_emoji):
+        return jsonify({'error': 'Not emoji'}), 400
+    if new_emoji in govno_emojis:
+        return jsonify({'error': 'Dublicate'}), 409
+    govno_emojis.append(new_emoji)
+    return jsonify(new_emoji), 201
+
+@app.route('/govno/<int:id>', methods=['DELETE'])
+def delete(id):
+    if id < 0 or id >= len(govno_emojis):
+        return jsonify({'error': 'Not found'}), 404
+    deleted_emoji = govno_emojis.pop(id)
+    return jsonify({'message': 'Emoji deleted', 'deleted_emoji': deleted_emoji}),200
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ manager.py
@@ -0,0 +1,74 @@
+import requests
+from abc import ABC, abstractmethod
+from emoji import is_emoji
+
+
+class ManagerInterface(ABC):
+    @abstractmethod
+    def update_emoji(self, id_emodji: int, new_emoji: str, endpoint: str, data: str):
+        pass
+
+    @abstractmethod
+    def add_new_emoji(self, emoji: str):
+        pass
+
+    @abstractmethod
+    def delete_emoji(self, id: int):
+        pass
+
+
+class Manager(ManagerInterface):
+    def __init__(self, base_usl: str):
+        self.base_url = base_usl
+
+    def _send_request(self, method: str, endpoint: str, data=None) -> requests.Response:
+        url = f'{self.base_url}/{endpoint}'
+        headers = {'Contex-Type': 'application/json'}
+
+        if method == 'PUT':
+            response = requests.put(url, json=data, headers=headers)
+        if method == 'POST':
+            response = requests.post(url, json=data, headers=headers)
+        if method == 'DELETE':
+            response = requests.delete(url, headers=headers)
+
+        return response
+
+    def add_new_emoji(self, emoji: str) -> str:
+        data = {'emoji': emoji}
+        response = self._send_request(method='POST', endpoint='govno', data=data)
+        if isinstance(response, requests.Response):
+            if response.status_code == 201:
+                print('SUCCESS!')
+            elif response.status_code == 409:
+                print('Error : Dublicate')
+            else:
+                print("I don't know what is this")
+
+        return 'ABOBUS'
+
+    def update_emoji(self, emoji_id: int, new_emoji: str, data: str) -> requests.Response:
+        data = {'emoji': new_emoji}
+        response = self._send_request(method='PUT', endpoint=f'govno/{emoji_id}', data=data)
+        if isinstance(response, requests.Response):
+            if response.status_code == 201:
+                print('SUCCESS!')
+            elif response.status_code == 404:
+                print('Error : Not Found')
+            else:
+                print("I don't know what is this")
+
+    def delete_emoji(self, id: int) -> requests.Response:
+        response = self._send_request(method='DELETE', endpoint=f'govno/{id}')
+        if isinstance(response, requests.Response):
+            if response.status_code == 201:
+                print('SUCCESS!')
+            else:
+                print("I don't know what is this")
+
+
+if __name__ == '__main__':
+    put_manager = Manager('http://127.0.0.1:5000')
+
+    new_emoji = '🐷'
+    put_manager.add_new_emoji(new_emoji)
